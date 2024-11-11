667bedbe32ba811aa430d3d50a23b019bf9254a1
MansourWorkingSpace <mansour.tech.contact@gmail.com>
2024-11-11 19:49:13 +0100

    css update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from "./navbar/navbar.component";
-import { ConnexionComponent } from "./connexion/connexion.component";
 import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule,CommonModule, NavbarComponent, ConnexionComponent],
+  imports: [RouterOutlet, FormsModule,CommonModule, NavbarComponent],
   exportAs: 'appRoot',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
