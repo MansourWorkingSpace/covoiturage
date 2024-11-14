7107a967ae0e94739cec5c506f437d2b4972de2b
MansourWorkingSpace <mansour.tech.contact@gmail.com>
2024-11-14 14:01:39 +0100

    service creation

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,9 @@
-import { NgStyle } from '@angular/common';
 import { Component, Output, EventEmitter } from '@angular/core';
 import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink,NgStyle],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
